6ac746d6aba06922bec75ac54148574c07a21884
akingse <48311823+akingse@users.noreply.github.com>
2024-04-07 10:04:35 +0800

    添加readme

--- Demo_OCC_QT/main.cpp
+++ Demo_OCC_QT/main.cpp
@@ -1,5 +1,6 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+//code from
 //https://www.bilibili.com/video/BV1N14y1N7q5/
 
 /*
@@ -7,22 +8,20 @@
 D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\inc
 //lib
 D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\win64\vc14\lib
-//dll (add to exe file dir) (same dll must add PATH)
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\win64\vc14\bin
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\qt5.11.2-vc14-64\bin
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\tcltk-86-64\bin
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\vtk-6.1.0-vc14-64\bin
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\tbb_2017.0.100\bin\intel64\vc14
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\openvr-1.14.15-64\bin\win64
-D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\freeimage-3.17.0-vc14-64\bin
-
+//dll add PATH=
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\win64\vc14\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\tbb_2017.0.100\bin\intel64\vc12;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\qt5.11.2-vc14-64\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\openvr-1.14.15-64\bin\win64;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\vtk-6.1.0-vc14-64\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\tcltk-86-64\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\freeimage-3.17.0-vc14-64\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\ffmpeg-3.3.4-64\bin;
+D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\freetype-2.5.5-vc14-64\bin;
 
 //open model
 D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\data
 */
-
-// QT creator UI Àı∑≈“Ï≥£
-// envir variable: QT_SCALE_FACTOR=1
 
 int main(int argc, char *argv[])
 {
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Demo_OCC_QT_sln
+
+[代码来源](https://www.bilibili.com/video/BV1N14y1N7q5/)
+
+### 项目版本
+
+occ7.6.0+vs2022+qt5.12.12
+
+[occ](https://dev.opencascade.org/release) 下载历史版本，7.6.0版本是exe文件，可直接安装；
+
+qt5.12 [官网](https://www.qt.io/offline-installers) [安装包](https://download.qt.io/archive/qt/5.12/5.12.12/qt-opensource-windows-x86-5.12.12.exe)
+
+vs2022 需安装插件(Qt visual studio tools)，并设置Path；
+
+### 项目配置
+
+添加依赖，[head](D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\inc)和[lib](D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\win64\vc14\lib)，本项目使用了绝对路径；
+
+动态库加载 [dll](D:\Alluser\akingse\TPL\OpenCASCADE-7.6.0-vc14-64\opencascade-7.6.0\win64\vc14\bin)，path添加vc14目录下的所有目录；
